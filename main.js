ca54df2ad8d537187e30f18f4d1624f2d5bc2515
MarioG8 <golikmariusz7@gmail.com>
2020-10-31 11:54:14 +0100

    Add new functions in js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const incomeEl = document.querySelector('#income');
 const expenseEl = document.querySelector('#expense');
 const allEl = document.querySelector('#all');
 const incomeList = document.querySelector('#income .list');
-const expenseList = document.querySelector('#outcome .list');
+const expenseList = document.querySelector('#expense .list');
 const allList = document.querySelector('#all .list');
 
 
@@ -51,7 +51,102 @@ allBtn.addEventListener('click', () => {
     inactive([incomeBtn, expenseBtn]);
 });
 
-// Sho/Hide Active/Inactive 
+
+addExpense.addEventListener('click', () => {
+    // If even one is empty => exit 
+    if (!expenseTitle.value || !expenseAmount.value) return;
+
+    // Save the entry to entry list 
+    let expense = {
+        type: 'expense',
+        title: expenseTitle.value,
+        amount: expenseAmount.value
+    }
+    ENTRY_LIST.push(expense);
+
+    updateUI();
+    clearInput([expenseTitle, expenseAmount])
+});
+
+addIncome.addEventListener('click', () => {
+    // If even one is empty => exit 
+    if (!incomeTitle.value || !incomeAmount.value) return;
+
+    // Save the entry to entry list 
+    let income = {
+        type: 'income',
+        title: incomeTitle.value,
+        amount: incomeAmount.value
+    }
+    ENTRY_LIST.push(income);
+
+    updateUI();
+    clearInput([incomeTitle, incomeAmount])
+});
+
+
+// Show/Hide Active/Inactive HelperFunc 
+function updateUI() {
+    income = calcTotal('income', ENTRY_LIST);
+    outcome = calcTotal('outcome', ENTRY_LIST);
+    balance = calcBalance(income, outcome);
+
+    // Update UI 
+    clearElement([expenseList, incomeList, allList]);
+
+    // Determine sign of balance 
+    let sign = (income >= outcome) ? '$' : '-$';
+
+    ENTRY_LIST.forEach((entry, index) => {
+        if (entry.type == 'expense') {
+            showEntry(expenseList, entry.type, entry.title, entry.amount, index)
+        } else if (entry.type == 'income') {
+            showEntry(incomeList, entry.type, entry.title, entry.amount, index)
+        }
+        showEntry(allList, entry.type, entry.amount, index)
+    });
+}
+
+function showEntry(list, type, title, amount, id) {
+
+    const entry = ` <li id = "${id}" class="${type}">
+                        <div class="entry">${title}: $${amount}</div>
+                        <div id="edit"></div>
+                        <div id="delete"></div>
+                    </li>`;
+
+    const position = "afterbegin";
+
+    list.insertAdjacentHTML(position, entry);
+}
+
+function clearElement(elements) {
+    elements.forEach(element => {
+        element.innerHTML = '';
+    });
+}
+
+function calcTotal(type, list) {
+    let sum = 0;
+
+    list.forEach(entry => {
+        if (entry.type == type) {
+            sum += entry.amount;
+        }
+    });
+
+    return sum;
+}
+
+function calcBalance(income, outcome) {
+    return income - outcome;
+}
+
+function clearInput(inputs) {
+    inputs.forEach(input => {
+        input.value = '';
+    });
+}
 
 function show(element) {
     element.classList.remove('hide');
